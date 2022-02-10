ee461fbea1619bde22fd5d05ca16a03e83942dff
jonas <jonasdeprund@gmail.com>
2022-02-10 16:53:29 +0100

    texture added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import fragment from './shaders/fragment.glsl';
 import vertex from './shaders/vertex.glsl';
-import testTexture from './img/water.jpg';
+import testTexture from './img/texture.jpg';
+import * as dat from 'dat.gui';
 
 console.log(testTexture);
 
@@ -34,10 +35,19 @@ export default class Sketch {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     this.time = 0;
+    this.setupSettings();
     this.resize();
     this.addObjects();
     this.render();
     this.setupResize();
+  }
+
+  setupSettings() {
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, 'progress', 0, 1, 0.001);
   }
 
   resize() {
@@ -53,12 +63,13 @@ export default class Sketch {
   }
 
   addObjects() {
-    this.geometry = new THREE.PlaneBufferGeometry(350, 350, 100, 100);
+    this.geometry = new THREE.PlaneBufferGeometry(300, 300, 100, 100);
 
     this.material = new THREE.ShaderMaterial({
       // wireframe: true,
       uniforms: {
         time: { value: 1.0 },
+        uProgress: { value: 1.0 },
         uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         resolution: { value: new THREE.Vector2() },
       },
@@ -73,6 +84,7 @@ export default class Sketch {
   render() {
     this.time += 0.05;
     this.material.uniforms.time.value = this.time;
+    this.material.uniforms.uProgress.value = this.settings.progress;
     this.mesh.rotation.x = this.time / 2000;
     this.mesh.rotation.y = this.time / 1000;
     this.renderer.render(this.scene, this.camera);
