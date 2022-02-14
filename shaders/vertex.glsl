fe616c8b3841eb6df3d66ad4ceca97068d3685ef
jonas <jonasdeprund@gmail.com>
2022-02-14 12:49:51 +0100

    zoom effect done

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,5 +1,7 @@
 uniform float time;
 uniform float uProgress;
+uniform vec2 uResolution;
+uniform vec2 uQuadSize;
 
 varying vec2 vUv;
 
@@ -8,6 +10,9 @@ void main(){
 
     vec4 defaultState = modelMatrix * vec4(position, 1.0);
     vec4 fullScreenState = vec4(position, 1.0);
+
+    fullScreenState.x *= uResolution.x / uQuadSize.x;
+    fullScreenState.y *= uResolution.y / uQuadSize.y;
     
     vec4 finalState = mix(defaultState, fullScreenState, uProgress);
     
