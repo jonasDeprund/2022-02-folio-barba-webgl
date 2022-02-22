35766c558536e1406b0456789a1f3607933e64a3
jonas <jonasdeprund@gmail.com>
2022-02-22 16:50:25 +0100

    page transition

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import ASScroll from '@ashthornton/asscroll';
 import * as THREE from 'three';
+import ASScroll from '@ashthornton/asscroll';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import fragment from './shaders/fragment.glsl';
 import vertex from './shaders/vertex.glsl';
-import testTexture from '../img/texture.jpg';
+// import testTexture from '../img/texture.jpg';
 import * as dat from 'dat.gui';
 import gsap from 'gsap';
 import barba from '@barba/core';
@@ -11,19 +11,19 @@ import barba from '@barba/core';
 export default class Sketch {
   constructor(options) {
     this.container = options.domElement;
-
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
 
     this.camera = new THREE.PerspectiveCamera(
-      80,
+      30,
       this.width / this.height,
       10,
       1000
     );
     this.camera.position.z = 600;
 
-    this.camera.fov = (2 * (Math.atan(this.height / 2 / 600) * 180)) / Math.PI;
+    this.camera.fov = (2 * Math.atan(this.height / 2 / 600) * 180) / Math.PI;
+    this.imagesAdded = 0;
 
     this.scene = new THREE.Scene();
 
@@ -32,9 +32,9 @@ export default class Sketch {
       alpha: true,
     });
     this.renderer.setPixelRatio(window.devicePixelRatio);
+    // this.renderer.setPixelRatio(2);
     this.container.appendChild(this.renderer.domElement);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
     this.materials = [];
 
     this.asscroll = new ASScroll({
@@ -44,36 +44,148 @@ export default class Sketch {
     this.asscroll.enable({
       horizontalScroll: !document.body.classList.contains('b-inside'),
     });
-
     this.time = 0;
-    // this.setupSettings();
+    // this.setupSettings()
+
     this.addObjects();
+    this.addClickEvents();
     this.resize();
     this.render();
+
     this.barba();
+
     this.setupResize();
   }
 
   barba() {
+    this.animationRunning = false;
+    let that = this;
     barba.init({
       transitions: [
         {
-          name: 'default-transition',
+          name: 'from-home-transition',
+          from: {
+            namespace: ['home'],
+          },
           leave(data) {
+            that.animationRunning = true;
+            that.asscroll.disable();
             return gsap.timeline().to(data.current.container, {
               opacity: 0,
+              duration: 0.5,
             });
           },
           enter(data) {
-            return gsap.timeline().to(data.next.container, {
-              opacity: 1,
+            that.asscroll = new ASScroll({
+              disableRaf: true,
+              containerElement: data.next.container.querySelector(
+                '[asscroll-container]'
+              ),
             });
+            that.asscroll.enable({
+              newScrollElements:
+                data.next.container.querySelector('.scroll-wrap'),
+            });
+            return gsap.timeline().from(data.next.container, {
+              opacity: 0,
+              onComplete: () => {
+                that.container.style.visibility = 'hidden';
+                that.animationRunning = false;
+              },
+            });
+          },
+        },
+        {
+          name: 'from-inside-page-transition',
+          from: {
+            namespace: ['inside'],
+          },
+          leave(data) {
+            that.asscroll.disable();
+            return gsap
+              .timeline()
+              .to('.curtain', {
+                duration: 0.3,
+                y: 0,
+              })
+              .to(data.current.container, {
+                opacity: 0,
+              });
+          },
+          enter(data) {
+            that.asscroll = new ASScroll({
+              disableRaf: true,
+              containerElement: data.next.container.querySelector(
+                '[asscroll-container]'
+              ),
+            });
+            that.asscroll.enable({
+              horizontalScroll: true,
+              newScrollElements:
+                data.next.container.querySelector('.scroll-wrap'),
+            });
+            // cleeaning old arrays
+            that.imageStore.forEach((m) => {
+              that.scene.remove(m.mesh);
+            });
+            that.imageStore = [];
+            that.materials = [];
+            that.addObjects();
+            that.resize();
+            that.addClickEvents();
+            that.container.style.visibility = 'visible';
+
+            return gsap
+              .timeline()
+              .to('.curtain', {
+                duration: 0.3,
+                y: '-100%',
+              })
+              .from(data.next.container, {
+                opacity: 0,
+              });
           },
         },
       ],
     });
   }
 
+  addClickEvents() {
+    this.imageStore.forEach((i) => {
+      i.img.addEventListener('click', () => {
+        let tl = gsap
+          .timeline()
+          .to(i.mesh.material.uniforms.uCorners.value, {
+            x: 1,
+            duration: 0.4,
+          })
+          .to(
+            i.mesh.material.uniforms.uCorners.value,
+            {
+              y: 1,
+              duration: 0.4,
+            },
+            0.1
+          )
+          .to(
+            i.mesh.material.uniforms.uCorners.value,
+            {
+              z: 1,
+              duration: 0.4,
+            },
+            0.2
+          )
+          .to(
+            i.mesh.material.uniforms.uCorners.value,
+            {
+              w: 1,
+              duration: 0.4,
+            },
+            0.3
+          );
+      });
+    });
+  }
   setupSettings() {
     this.settings = {
       progress: 0,
@@ -88,6 +200,8 @@ export default class Sketch {
     this.renderer.setSize(this.width, this.height);
     this.camera.aspect = this.width / this.height;
     this.camera.updateProjectionMatrix();
+
+    this.camera.fov = (2 * Math.atan(this.height / 2 / 600) * 180) / Math.PI;
 
     this.materials.forEach((m) => {
       m.uniforms.uResolution.value.x = this.width;
@@ -133,7 +247,7 @@ export default class Sketch {
 
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.mesh.scale.set(300, 300, 1);
-    // this.scene.add(this.mesh);
+    // this.scene.add( this.mesh );
     this.mesh.position.x = 300;
 
     this.images = [...document.querySelectorAll('.js-image')];
@@ -147,75 +261,29 @@ export default class Sketch {
 
       m.uniforms.uTexture.value = texture;
 
-      img.addEventListener('mouseover', () => {
-        this.tl = gsap
-          .timeline()
-          .to(m.uniforms.uCorners.value, {
-            x: 1,
-            duration: 0.4,
-          })
-          .to(
-            m.uniforms.uCorners.value,
-            {
-              y: 1,
-              duration: 0.4,
-            },
-            0.1
-          )
-          .to(
-            m.uniforms.uCorners.value,
-            {
-              z: 1,
-              duration: 0.4,
-            },
-            0.2
-          )
-          .to(
-            m.uniforms.uCorners.value,
-            {
-              w: 1,
-              duration: 0.4,
-            },
-            0.3
-          );
-      });
-
-      img.addEventListener('mouseout', () => {
-        this.tl = gsap
-          .timeline()
-          .to(m.uniforms.uCorners.value, {
-            x: 0,
-            duration: 0.4,
-          })
-          .to(
-            m.uniforms.uCorners.value,
-            {
-              y: 0,
-              duration: 0.4,
-            },
-            0.1
-          )
-          .to(
-            m.uniforms.uCorners.value,
-            {
-              z: 0,
-              duration: 0.4,
-            },
-            0.2
-          )
-          .to(
-            m.uniforms.uCorners.value,
-            {
-              w: 0,
-              duration: 0.4,
-            },
-            0.3
-          );
-      });
+      // img.addEventListener('mouseout',()=>{
+      //     this.tl = gsap.timeline()
+      //     .to(m.uniforms.uCorners.value,{
+      //         x:0,
+      //         duration: 0.4
+      //     })
+      //     .to(m.uniforms.uCorners.value,{
+      //         y:0,
+      //         duration: 0.4
+      //     },0.1)
+      //     .to(m.uniforms.uCorners.value,{
+      //         z:0,
+      //         duration: 0.4
+      //     },0.2)
+      //     .to(m.uniforms.uCorners.value,{
+      //         w:0,
+      //         duration: 0.4
+      //     },0.3)
+      // })
 
       let mesh = new THREE.Mesh(this.geometry, m);
       this.scene.add(mesh);
-      this.mesh.scale.set(bounds.width, bounds.height, 1);
+      mesh.scale.set(bounds.width, bounds.height, 1);
       return {
         img: img,
         mesh: mesh,
@@ -228,25 +296,23 @@ export default class Sketch {
   }
 
   setPosition() {
-    console.log(this.asscroll.currentPos);
-    this.imageStore.forEach((o) => {
-      o.mesh.position.x =
-        -this.asscroll.currentPos + o.left - this.width / 2 + o.width / 2;
-      o.mesh.position.y = -o.top + this.height / 2 - o.height / 2;
-    });
+    // console.log(this.asscroll.currentPos)
+    if (!this.animationRunning) {
+      this.imageStore.forEach((o) => {
+        o.mesh.position.x =
+          -this.asscroll.currentPos + o.left - this.width / 2 + o.width / 2;
+        o.mesh.position.y = -o.top + this.height / 2 - o.height / 2;
+      });
+    }
   }
 
   render() {
     this.time += 0.05;
     this.material.uniforms.time.value = this.time;
-    // this.material.uniforms.uProgress.value = this.settings.progress;
+
     this.asscroll.update();
     this.setPosition();
-    // this.tl.progress(this.settings.progress);
-    this.mesh.rotation.x = this.time / 2000;
-    this.mesh.rotation.y = this.time / 1000;
     this.renderer.render(this.scene, this.camera);
-
     requestAnimationFrame(this.render.bind(this));
   }
 }
